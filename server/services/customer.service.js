7a9aea92eccab8bd953ead5dfbc2c232f7844180
yoseph <yoseph3145@gmail.com>
2025-09-03 13:25:56 -0700

    Update customer.service.js

--- server/services/customer.service.js
+++ server/services/customer.service.js
@@ -1,6 +1,5 @@
 const { pool } = require('../config/db.config');
 
-// Get all insureds
 exports.getAllInsured = async () => {
   const [rows] = await pool.query(
     'SELECT insured_id, insured_name FROM insured ORDER BY insured_name ASC'
@@ -8,7 +7,6 @@ exports.getAllInsured = async () => {
   return rows;
 };
 
-// Get insured by ID (only id + name available in schema)
 exports.getInsuredById = async (insuredId) => {
   const [rows] = await pool.query(
     `SELECT insured_id, insured_name 
